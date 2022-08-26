d8e1fc413f3b13ed4b232edd60cc658e4529e3b8
Geckkou <alexandre.asr2001@hotmail.com>
2022-08-26 19:24:08 -0300

    deleting the private subnet

--- subnet.tf
+++ subnet.tf
@@ -16,18 +16,3 @@ resource "aws_subnet" "public_subnet" {
 
   map_public_ip_on_launch = true
 }
-
-resource "aws_subnet" "private_subnet" {
-  depends_on = [
-    aws_vpc.vpc
-  ]
-
-  vpc_id     = aws_vpc.vpc.id
-  cidr_block = "192.168.1.0/24"
-
-  availability_zone = "us-east-1b"
-
-  tags = {
-    Name = "private_subnet"
-  }
-}
