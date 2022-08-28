8d7463119b4c37aaa4586df2400f4e4a6d538196
Geckkou <alexandre.asr2001@hotmail.com>
2022-08-27 23:06:39 -0300

    update user-data-nginx.sh

--- user-data-nginx.sh
+++ user-data-nginx.sh
@@ -2,8 +2,10 @@
 cd /etc/yum.repos.d/
 sudo sed -i 's/mirrorlist/#mirrorlist/g' /etc/yum.repos.d/CentOS-*
 sudo sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /etc/yum.repos.d/CentOS-*
+
 sudo yum update -y
 sudo yum upgrade -y
+
 sudo dnf install nginx -y
 sudo systemctl enable nginx
 sudo systemctl start nginx
